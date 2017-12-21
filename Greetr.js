fe49a46bc23a3a130e1f381c99a88a1685334674
Simone Taeggi <simone.taeggi@leovegas.com>
2017-12-21 17:59:17 +0100

    Property e MEtodi Concatenabili

--- Greetr.js
+++ Greetr.js
@@ -5,9 +5,79 @@
     return new Greetr.init(firstName, lastName, language);
   }
   
+  // linguaggi supportati
+  var supportedLangs = ['en', 'es'];
+  
+  var greetings = {
+    en: 'Hello',
+    es: 'Hola'
+  };
+  
+  var formalGreetings = {
+    en: 'Greetings',
+    es: 'Saludos'
+  };
+  
+  var logMessages = {
+    en: 'Logged in',
+    es: 'Iniciò sesiòn'
+  };
+  
   // definisco il Protype della classe dove andrò a definire tutti i metodi
   Greetr.prototype = {
     
+    fullName: function() {
+      return this.firstName + ' ' + this.lastName;
+    },
+    
+    validate: function() {
+      if (supportedLangs.indexOf(this.language) === -1) {
+        throw 'Invalid language';
+      }
+    },
+    
+    greeting: function() {
+      return greetings[this.language] + ' ' + this.firstName + '!';
+    },
+    
+    formalGreeting: function() {
+      return formalGreetings[this.language] + ', ' + this.fullName();
+    },
+    
+    greet: function(formal) {
+      var msg;
+      
+      // if undefined or null it will be coerced to 'false'
+      if (formal) {
+        msg = this.formalGreeting();
+      } else {
+        msg = this.greeting();
+      }
+      // se la console è aperta
+      if (console) {
+        console.log(msg);
+      }
+      
+      // 'this' refers to the calling object at execution time
+      // makes the method chainable
+      return this;
+    },
+    
+    log: function() {
+      // se la console è aperta
+      if (console) {
+        console.log(logMessages[this.language] + ': ' + this.fullName());
+      }
+      return this;
+    },
+    
+    setLang: function(lang) {
+      this.language = lang;
+      
+      this.validate();
+      
+      return this;
+    }
   };
   
   // definisco la pty init
