8cbbcdd5ff6cf68a864a4c7255c26be0e2ebf802
Simone Taeggi <simone.taeggi@leovegas.com>
2017-12-21 18:12:58 +0100

    Aggiunto supporto a jQuery

--- Greetr.js
+++ Greetr.js
@@ -77,6 +77,28 @@
       this.validate();
       
       return this;
+    },
+    
+    HTMLGreeting: function(selector, formal) {
+      if (!$) {
+        throw 'jQuery not loaded';
+      }
+      
+      if (!selector) {
+        throw 'Missing jQuery selector';
+      }
+      
+      var msg;
+      if (formal) {
+        msg = this.formalGreeting();
+      } else {
+        msg = this.greeting();
+      }
+      
+      $(selector).html(msg);
+      
+      return this;
+      
     }
   };
   
