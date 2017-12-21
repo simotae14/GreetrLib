dffdc91690e12147403896836af57a6f1c27b4e1
Simone Taeggi <simone.taeggi@leovegas.com>
2017-12-21 18:59:36 +0100

    commenti

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+// crea un nuovo oggetto (l'architettura ti permette di non dover usare la keyword 'new')
 var g = G$('John', 'Doe');
 
+// usa i nostri metodi concatenabili
 g.greet().setLang('es').greet(true).log();
 
 // handler sul pulsante login
+// usa il nostro oggetto al click sul bottone di login
 $('#login').click(function() {
-  
+  // creiamo un nuovo oggetto 'Greetr' (dobbiamo assicurarci di sapere il nome dal login)
   var loginGrtr = G$('John', 'Doe');
   
   $('#logindiv').hide();
   
+  // crea un Saluto HTML, passando come selettore '#greeting' e il linguaggio a tua scelta, e logga anche il
+  // messaggio di benvenuto
   loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
 });
