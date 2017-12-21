aa2c8738faa9bbe44bbc381586a638033736689b
Simone Taeggi <simone.taeggi@leovegas.com>
2017-12-21 16:06:08 +0100

    Oggetto e suo Prototype

--- Greetr.js
+++ Greetr.js
@@ -1,4 +1,26 @@
 // IIFE
 (function (global, $) {
+  // funzione che crea saluto
+  var Greetr = function (firstName, lastName, language) {
+    return new Greetr.init(firstName, lastName, language);
+  }
   
+  // definisco il Protype della classe dove andrò a definire tutti i metodi
+  Greetr.prototype = {
+    
+  };
+  
+  // definisco la pty init
+  Greetr.init = function (firstName, lastName, language) {
+    var self = this;
+    // setto i valori delle pty tenendo conto anche dei loro valori di default
+    self.firstName = firstName || '';
+    self.lastName = lastName || '';
+    self.language = language || 'en';
+  }
+  
+  // definisco anche un prototype del costruttore init che sarà il prototype usato dai metodi della libreria
+  Greetr.init.prototype = Greetr.prototype;
+  
+  global.Greetr = global.G$ = Greetr;
 }(window, jQuery));
